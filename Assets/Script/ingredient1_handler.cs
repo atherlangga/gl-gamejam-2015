bdf94ef7c411aae4aedbc736f558f306664a693b
Ruben Ramirez <ramirez.ruben.dario10@gmail.com>
2015-11-13 23:16:24 +0700

    +PopUP
    Still basics QUADS with color textures

---
+++ Assets/Script/ingredient1_handler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ingredient1_handler : MonoBehaviour {
+
+	public GameObject ingredient;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+
+	}
+
+	void OnMouseUp(){
+		ingredient.GetComponent<Renderer> ().enabled = true;
+	}
+
+
+}
