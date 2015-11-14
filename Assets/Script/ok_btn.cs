827cf02072aa221f0ac90ecd6fa31ead7ecab82a
Ruben Ramirez <ramirez.ruben.dario10@gmail.com>
2015-11-14 10:27:02 +0700

    Merged scripts and added Restaurant

--- Assets/Script/ok_btn.cs
+++ Assets/Script/ok_btn.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class ok_btn : MonoBehaviour {
 
+	public GameManager gameManager;
+
+
 	public static int currentOrder;
 	public static bool isRebus;
 	public static bool isGoreng;
@@ -14,8 +17,14 @@ public class ok_btn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		print (currentOrder);
-		print ("Is rebus: " + isRebus);
-		print ("Is Goreng: " + isGoreng);
+		//print (currentOrder);
+		//print ("Is rebus: " + isRebus);
+		//print ("Is Goreng: " + isGoreng);
 	}
+
+	void OnMouseUp(){
+		gameManager.ServeCustomerResult1 (currentOrder);
+	}
+
+
 }
